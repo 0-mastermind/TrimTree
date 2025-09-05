2f54e4f61d55498cd69f47671b6f0b8d14759f51
himanshu <himanshu32505@gmail.com>
2025-09-05 16:07:50 +0530

    added : route protection

--- Backend/src/middleware/auth.middleware.ts
+++ Backend/src/middleware/auth.middleware.ts
@@ -1,6 +1,7 @@
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { ApiError } from "../utils/ApiError.js";
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
@@ -9,21 +10,40 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
   const token = req.cookies.token;
 
   if (!token) {
-    return res.status(401).json({
-      success: false,
-      message: "No token provided, authorization denied",
-    });
+   throw new ApiError(401 , "No token, authorization denied");
   }
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET) as { _id: string; role: string };
+    const decoded = jwt.verify(token, JWT_SECRET) as { _id: string; role: string; branch : string };
     req.userId = decoded._id;
     req.role = decoded.role;
+    req.branch = decoded.branch;
     next();
   } catch (err) {
-    return res.status(401).json({
-      success: false,
-      message: "Token is not valid",
-    });
+    throw new ApiError(401 , "Invalid token, authorization denied");
   }
 }
+
+
+export const staffMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (req.role !== "STAFF") {
+    throw new ApiError(403, "Access denied: Staff only");
+  }
+  next();
+};
+
+
+export const managerMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (req.role !== "MANAGER") {
+    throw new ApiError(403, "Access denied: Manager only");
+  }
+  next();
+};
+
+
+export const adminMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (req.role !== "ADMIN") {
+    throw new ApiError(403, "Access denied: Admin only");
+  }
+  next();
+};
