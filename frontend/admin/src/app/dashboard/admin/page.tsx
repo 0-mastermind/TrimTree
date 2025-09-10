428b2feae269274114adb692460cd321bab879a0
dinesh <dchandrap973@gmail.com>
2025-09-10 17:54:42 +0530

    added: nav and breadcrumd

---
+++ frontend/admin/src/app/dashboard/admin/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="">
+      <div className="h-[1200px]">
+        afsfasdfsd
+      </div>
+    </div>
+  )
+}
+
+export default page
