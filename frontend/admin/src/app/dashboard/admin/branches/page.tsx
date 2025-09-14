922893e3febe109a37cc54efd73e36567bd79d1d
dinesh <dchandrap973@gmail.com>
2025-09-14 08:57:58 +0530

    added: create branch and add staff routes

--- frontend/admin/src/app/dashboard/admin/branches/page.tsx
+++ frontend/admin/src/app/dashboard/admin/branches/page.tsx
@@ -18,7 +18,7 @@ const page = () => {
         <div className="flex justify-between items-center">
           <h1 className="text-2xl md:text-3xl font-semibold">Your Branches</h1>
           <button
-            className="text-xs mt-4 cursor-pointer transition-all bg-green-600 text-white px-6 py-2 rounded-lg border-green-700 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px] flex gap-2"
+            className="text-xs mt-4 cursor-pointer transition-all bg-green-600 text-white px-6 py-2 rounded-lg border-green-700 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px] flex gap-2 items-center"
             onClick={() =>
               redirectToURI("/dashboard/admin/branches/add-branch")
             }>
