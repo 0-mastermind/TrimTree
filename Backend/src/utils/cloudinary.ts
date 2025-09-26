046260724178b26ba5ee4b47a1f7aebf8e94e031
himanshu <himanshu32505@gmail.com>
2025-09-26 14:26:23 +0530

    variable change

--- Backend/src/utils/cloudinary.ts
+++ Backend/src/utils/cloudinary.ts
@@ -3,14 +3,14 @@ import { v2 as cloudinary, type UploadApiResponse } from "cloudinary";
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 
 const uploadOnCloudinary = async (
   localFilePath: string,
   folderName: string
 ): Promise<UploadApiResponse> => {
-  if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
+  if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_SECRET_KEY) {
     throw new Error("Cloudinary is not configured");
   }
 
