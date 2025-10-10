e6e9dc00e9386942f80f63006a330768dd3fc7b2
dinesh <dchandrap973@gmail.com>
2025-10-11 02:02:21 +0530

    Add health check and update remote image patterns in configuration

--- frontend/admin/next.config.ts
+++ frontend/admin/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://res.cloudinary.com/dblfmkwvf/**")]
+    remotePatterns: [new URL("https://res.cloudinary.com/dg7aip8fs/**")]
   }
 };
 
