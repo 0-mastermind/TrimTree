6f64e68678ea30850e566ac1c35cfe66b62ccb63
Himanshu Gaura <himanshu32505@gmail.com>
2025-10-23 14:27:58 +0530

    updated : config

--- landingpage/next.config.ts
+++ landingpage/next.config.ts
@@ -1,18 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "/**",  
-      },
-    ],
+    domains: ["randomuser.me", "res.cloudinary.com"],
   },
 };
 
