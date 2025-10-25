d1c3ec8367536175a7253e0f82109aa53b7b4989
dinesh <dchandrap973@gmail.com>
2025-10-25 20:06:31 +0530

    sync

--- landingpage/src/components/reviews/Reviews.tsx
+++ landingpage/src/components/reviews/Reviews.tsx
@@ -1,15 +1,24 @@
+import { data } from "./data";
+
 const Reviews = () => {
   return (
     <section className="my-10 py-4">
       <h1 className="my-4 text-4xl md:text-5xl max-w-[700px] text-center mx-auto text-[var(--text-primary)] font-semibold">
         What our clients say about Trim Tree Salon&apos;s services
       </h1>
-      
+
       {/* Bento Grid */}
       <div className="grid grid-rows-12 grid-cols-3 gap-2">
+        {
+          data.map((item) => (
+            <div className="bg-red-500" key={item.id}>
+              ad
+            </div>
+          ))
+        }      
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Reviews
+export default Reviews;
